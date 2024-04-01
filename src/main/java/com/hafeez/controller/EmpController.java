50217b08e6de6e3cee606c085dc294796e686034
hafeez8165526 <45596435+hafeez8165526@users.noreply.github.com>
2024-04-01 17:25:33 +0530

    Update EmpController.java

--- src/main/java/com/hafeez/controller/EmpController.java
+++ src/main/java/com/hafeez/controller/EmpController.java
@@ -8,6 +8,6 @@ public class EmpController {
 	
 	@GetMapping("test")
 	public String test() {
-		return "<h1>hello hai 111 git chnage again</h1>";
+		return "<h1>hello hai 112 git chnage again</h1>";
 	}
 }
